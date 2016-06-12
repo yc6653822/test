891850e2b1c5889213d3832f3d6f7b4ce149636c
yc6653822 <1056771179@qq.com>
2016-06-12 09:02:20 +0800

    eee

--- src/JavaDD.java
+++ src/JavaDD.java
@@ -1,10 +1,26 @@
-
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.NumberFormat;
 
 public class JavaDD {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("good");
+
+		double a = 0.33333d;
+		double b = 0.22222d;
+
+		NumberFormat format = NumberFormat.getInstance();
+		format.setMaximumFractionDigits(1);
+
+		System.out.println(format.format(a - b));
+
+		BigDecimal d = new BigDecimal(a);
+		BigDecimal e = new BigDecimal(b);
+		MathContext context = new MathContext(4);
+		String c = d.subtract(e, context).toString();
+
+		System.out.println(c);
+		System.out.println("what the fuck");
 
 	}
 
